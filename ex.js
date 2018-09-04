d030b90f11313f91990a338b9e0a6c5c48a0c3e0
Basavaraj Mallikarjuna Teli <BT00551246IPC@TechmIPC.com>
2018-09-04 12:58:20 +0530

    updated ex.js package

--- ex.js
+++ ex.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://basavaraj:basu123@ds133388.mlab.com:33388/abt";
+ 
 
 app.get('/', function (req, res) {
-
-  res.send('Hello World');
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+   var dbo = db.db("abt");
+  
+  dbo.collection("user").find({}).toArray(function(err,results) {
+     if (err) throw err;
+    res.json(results)
+    db.close();
+  });
+});
+  
 
 })
 
